810a4c9de51ddb5335cec530c2e730eed66966cd
Elliot Kim <elliotkim916@gmail.com>
2018-08-02 15:52:49 -0700

    added code to visualize detection results, to display face landmarks, and fetch images from url to blobs and convert those blobs to HTMLImage elements

--- public/index.js
+++ public/index.js
@@ -5,32 +5,55 @@ const MODEL_URL = '/models';
 await faceapi.loadModels(MODEL_URL);
 
 // If wanting to only load specific models
-await faceapi.loadFaceDetectionModel(MODEL_URL);
-await faceapi.loadFaceLandmarkModel(MODEL_URL);
-await faceapi.loadFaceRecognitionModel(MODEL_URL);
+// await faceapi.loadFaceDetectionModel(MODEL_URL);
+// await faceapi.loadFaceLandmarkModel(MODEL_URL);
+// await faceapi.loadFaceRecognitionModel(MODEL_URL);
 
 // To detect the face’s bounding boxes of an input with a score > minScore we simply say:
 const minConfidence = 0.8;
 const fullFaceDescriptions = await faceapi.allFaces(input, minConfidence);
 
-// Gaining access to user's webcam (audio & video)
-const constraints = {
-  audio: true,
-  video: {
-    width: 1280,
-    height: 720
-  }
-};
+// We can visualize the detection results by drawing the bounding boxes into a canvas:
+fullFaceDescriptions.forEach((fd, i) => {
+  faceapi.drawDetection(CanvasGradient, fd.detection, {withScore: true})
+})
 
-navigator.mediaDevices.getUserMedia(constraints)
-  .then(function (mediaStream) {
-    let video = document.querySelector('video');
-    video.srcObject = mediaStream;
-    video.onloadedmetadata = function(e) {
-      e.preventDefault();
-      video.play();
-    };
-  })
-  .catch(function(err) {
-    console.log(err.message);
-  });
+// The face landmarks can be displayed as follows:
+fullFaceDescriptions.forEach((fd, i) => {
+  faceapi.drawLandmarks(canvas, fd.landmarks, {drawLines: true})
+})
+
+// fetch images from url as blobs
+const blobs = await Promise.all(
+  ['sheldon.png', 'raj.png', 'leonard.png', 'howard.png'].map(
+    async uri => (await fetch(uri)).blob()
+  )
+)
+
+// convert blobs to HTMLImage elements
+const images = await Promise.all(blobs.map(
+  blob => faceapi.bufferToImage(blob)
+))
+
+
+// Gaining access to user's webcam (audio & video)
+// const constraints = {
+//   audio: true,
+//   video: {
+//     width: 1280,
+//     height: 720
+//   }
+// };
+
+// navigator.mediaDevices.getUserMedia(constraints)
+//   .then(function (mediaStream) {
+//     let video = document.querySelector('video');
+//     video.srcObject = mediaStream;
+//     video.onloadedmetadata = function(e) {
+//       e.preventDefault();
+//       video.play();
+//     };
+//   })
+//   .catch(function(err) {
+//     console.log(err.message);
+//   });
